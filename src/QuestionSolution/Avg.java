36fabdde0c27b1a370e9137286353ca49c60fd90
Vipul <vipulnitsri2000@gmail.com>
2021-12-27 22:55:58 +0530

    first commit

---
+++ src/QuestionSolution/Avg.java
@@ -0,0 +1,24 @@
+/*Write a program to input a name(as a single character) and marks of
+  three tests as m1, m2, and m3 of a student considering all the three marks
+  have been given in integer format. */
+
+
+package QuestionSolution;
+
+import java.util.Scanner;
+
+public class Avg {
+    public static void main(String[] args) {
+
+        int m1,m2,m3;
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        m1=s.nextInt();
+        m2=s.nextInt();
+        m3=s.nextInt();
+        int avg=(m1+m2+m3)/3;
+        System.out.println(str);
+        System.out.print(avg);
+    }
+
+}
