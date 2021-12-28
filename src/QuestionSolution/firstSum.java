82da654a2ab326ad763adcdac225e833abc6aeb4
Vipul <vipulnitsri2000@gmail.com>
2021-12-28 20:59:44 +0530

    sum of N number is file is added

---
+++ src/QuestionSolution/firstSum.java
@@ -0,0 +1,21 @@
+package QuestionSolution;
+
+import java.util.Scanner;
+
+public class firstSum {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+
+        int i=0;
+        int sum=0;
+
+        while (i<=n){
+            sum=sum+i;
+            i=i+1;
+        }
+        System.out.println(sum);
+
+    }
+}
