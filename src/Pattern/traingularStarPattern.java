6f82fbe1eaed47a7d7dff153f8e3c47f2f1c342c
Vipul <vipulnitsri2000@gmail.com>
2022-01-07 22:15:19 +0530

    triangular star pattern

---
+++ src/Pattern/traingularStarPattern.java
@@ -0,0 +1,31 @@
+/*Print the following pattern for the given N number of rows.
+        Pattern for N = 4
+        *
+        **
+        ***
+        **** */
+
+
+
+
+package Pattern;
+import java.util.Scanner;
+
+public class traingularStarPattern {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+
+        for(int row=1; row<=n; row++){
+
+            for(int col=1; col<=row; col++){
+
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+        }
+    }
+}
